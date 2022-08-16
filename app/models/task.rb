c9b76c9a758df93ca2d190a0f5e22ee06f6d9a62
Lauren Hunter <laurenbhunter00@gmail.com>
2022-08-16 11:59:32 +0200

    add task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,2 @@
-class CreateTasks < ActiveRecord::Migration[7.0]
-  def change
-    create_table :tasks do |t|
-      t.string :title
-      t.string :details
-      t.string :completed
-
-      t.timestamps
-    end
-  end
+class Task < ApplicationRecord
 end
